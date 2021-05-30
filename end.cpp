31a6d7326e7c762e16c5479c3d3ea69e527697eb
Tom <tom@contextfree.info>
2021-05-30 12:08:06 -0700

    Simplify

--- end.cpp
+++ end.cpp
@@ -21,27 +21,22 @@ auto split(const std::string& string, char delimiter)
 }
 
 auto main(int argc, const char** argv) -> int {
-    constexpr auto tropic = 23.43651;
     auto args = std::vector(argv, argv + argc);
     auto file = std::ifstream(args.at(1));
     if (!file) throw std::runtime_error("bad file");
     auto line = std::string();
     auto total_north = std::int64_t(0);
-    auto total_tropic = std::int64_t(0);
     auto total_south = std::int64_t(0);
     while (std::getline(file, line)) {
         auto fields = split(line, '\t');
         auto latitude = std::stod(fields.at(4));
         auto population = std::stoll(fields.at(14));
-        if (latitude > tropic) {
+        if (latitude > 0) {
             total_north += population;
-        } else if (latitude < -tropic) {
-            total_south += population;
         } else {
-            total_tropic += population;
+            total_south += population;
         }
     }
     std::cout << "North:  " << total_north << "\n";
-    std::cout << "Tropic: " << total_tropic << "\n";
     std::cout << "South:  " << total_south << "\n";
 }
