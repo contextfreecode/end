6ed906c7d5b73d5bc1ae9abd089695bd9d0fba7c
Tom <tom@contextfree.info>
2021-06-01 20:59:13 -0700

    Work on scala

--- s3end/end/src/main/scala/Main.scala
+++ s3end/end/src/main/scala/Main.scala
@@ -1,5 +1,24 @@
-@main def hello: Unit = 
-  println("Hello world!")
-  println(msg)
+import scala.util.control.Breaks._
 
-def msg = "I was compiled by Scala 3. :)"
+class Region(
+  val name: String,
+  population: Long,
+  val southEdge: Double,
+)
+
+def tally(file: scala.io.Source) =
+  breakable {
+    for (line <- file.getLines)
+      val fields = line.split('\t')
+      println(fields.length)
+      break
+  }
+
+@main def hello(name: String): Unit =
+  scala.util.Using(scala.io.Source.fromFile(name)) { file =>
+    val regions = List(
+      Region(name = "North", population = 0, southEdge = 0),
+      Region(name = "South", population = 0, southEdge = -90),
+    )
+    tally(file)
+  }
