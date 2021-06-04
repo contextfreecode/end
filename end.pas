d5a126b6acc4c59ad57bb4f9ffa18843bd5c51a2
Tom <tom@contextfree.info>
2021-06-04 06:29:17 -0700

    Work pascal

--- end.pas
+++ end.pas
@@ -1,5 +1,39 @@
 program BookEnd;
+{$mode objfpc}
+
+uses Sysutils;
+
+type
+  TRegion = record
+    Name: UTF8String;
+    Population: Int64;
+    SouthEdge: Double;
+  end;
+
+var
+  inputFile: TextFile;
+  region: TRegion;
+  regions: array of TRegion = (
+    (Name: 'North'; Population: 0; SouthEdge: 0),
+    (Name: 'South'; Population: 0; SouthEdge: -90)
+  );
+
+procedure Tally(var inputFile: TextFile; var regions: array of TRegion);
+begin
+  WriteLn('Hi!');
+end;
 
 begin
-  WriteLn('Hi!')
+  if ParamCount() < 1 then raise Exception.Create('No file name');
+  AssignFile(inputFile, ParamStr(1));
+  WriteLn(ParamStr(1));
+  Reset(inputFile);
+  try
+    Tally(inputFile, regions);
+  finally
+    Close(inputFile);
+  end;
+  for region in regions do begin
+    WriteLn(region.Name, ': ', region.Population);
+  end;
 end.
